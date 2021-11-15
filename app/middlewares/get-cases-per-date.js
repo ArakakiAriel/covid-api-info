57ed44d9a7cbf48389cc59754d44d55739415984
ArielArakaki <arakaki.arielkenji@gmail.com>
2021-11-15 21:36:57 +0900

    Version 2 of API
    Deleted recovered, actives data because the database no longer shows that data correctly.
    Added population data instead.

--- app/middlewares/get-cases-per-date.js
+++ app/middlewares/get-cases-per-date.js
@@ -22,13 +22,10 @@ module.exports.getCasesPerDate = async (req, res, next) => {
             // The SQL query to run
             let normalizedCountriesCases = normalizeCountries(config.bigQuery.countries_to_normalize, "aux");
             let normalizedCountriesCases2 = normalizeCountries(config.bigQuery.countries_to_normalize, "c");
-            const sqlQuery = `SELECT cases.country_region as country, (SUM(cases.latitude * cases.confirmed)/SUM(cases.confirmed)) as latitude, 
-            (SUM(cases.longitude * cases.confirmed)/SUM(cases.confirmed)) as longitude,SUM(COALESCE(cases.confirmed, 0)) as total_confirmed, 
-            SUM(COALESCE(cases.deaths, 0)) as total_deaths, SUM(COALESCE(cases.recovered, 0)) as total_recovered, 
-            SUM(COALESCE(cases.active, 0)) as total_active_cases, MAX(cases.date) as updated_date
+            const sqlQuery = `SELECT casea.country_region, casea.total_confirmed , casea.total_deaths, casea.updated_date, caseb.country_code, caseb.population,caseb.population_male, caseb.population_female, caseb.population_density, caseb.latitude, caseb.longitude  FROM (SELECT cases.country_region as country_region, SUM(COALESCE(cases.confirmed, 0)) as total_confirmed, 
+            SUM(COALESCE(cases.deaths, 0)) as total_deaths,  MAX(cases.date) as updated_date
             FROM
-                (SELECT ${normalizedCountriesCases} as country_region, aux.latitude, aux.longitude,aux.confirmed,aux.deaths,aux.recovered,
-                aux.active,aux.date
+                (SELECT ${normalizedCountriesCases} as country_region,aux.confirmed,aux.deaths,aux.date
                 FROM \`bigquery-public-data.covid19_jhu_csse.summary\` aux) cases
             INNER JOIN (
                 SELECT ${normalizedCountriesCases2} as country, MAX(c.date) as maxdate
@@ -36,9 +33,13 @@ module.exports.getCasesPerDate = async (req, res, next) => {
                 WHERE c.date <= '${formatedDate}'
                 GROUP BY country
             ) lcases ON upper(cases.country_region)  = upper(lcases.country) AND cases.date = lcases.maxdate
-            GROUP BY country
+            GROUP BY country_region
             HAVING total_confirmed > 0
-            ORDER BY total_confirmed desc;`;
+            ORDER BY total_confirmed desc) casea
+            LEFT JOIN (
+                SELECT upper(country_name) as country, country_code, population, population_male, population_female, population_density, latitude, longitude FROM \`bigquery-public-data.covid19_open_data.covid19_open_data\` WHERE aggregation_level = 0 AND date = '${formatedDate}'
+            ) caseb ON upper(casea.country_region) = upper(caseb.country) 
+            ORDER BY total_confirmed DESC;`;
             const options = {
             query: sqlQuery,
             // Location must match that of the dataset(s) referenced in the query.
@@ -52,43 +53,55 @@ module.exports.getCasesPerDate = async (req, res, next) => {
             if(!globalCases){
                 return setResponseWithError(res, constants.NOT_FOUND_ERROR, messages.NOT_RESULT_FOUND);
             }
-
+            //Falta modificar esto ahora
             let globalConfirmedCases = 0;
-            let globalActiveCases = 0;
-            let globalRecovered = 0;
+                //let globalActiveCases = 0;
+                //let globalRecovered = 0;
             let globalDeaths = 0;
+            let globalConcernedPopulation = 0;
+            let globalMaleConcernedPopulation = 0;
+            let globalFemaleConcernedPopulation = 0;
             for(let i = 0; i < globalCases.length; i++){
                 globalConfirmedCases += globalCases[i].total_confirmed;
-                globalActiveCases += globalCases[i].total_active_cases;
-                globalRecovered += globalCases[i].total_recovered;
+                    //globalActiveCases += globalCases[i].total_active_cases;
+                    //globalRecovered += globalCases[i].total_recovered;
                 globalDeaths += globalCases[i].total_deaths;
+                globalConcernedPopulation += globalCases[i].population;
+                globalMaleConcernedPopulation  += globalCases[i].population_male;
+                globalFemaleConcernedPopulation  += globalCases[i].population_female;
             }
 
             //Data normalizer
             for(let i = 0; i < globalCases.length; i++){
-                globalCases[i].country = globalCases[i].country.toUpperCase();
+                globalCases[i].country_region = globalCases[i].country_region.toUpperCase();
                 globalCases[i].position_in_table = i + 1;
-                let activeCases = (globalCases[i].total_active_cases == 0 ? globalCases[i].total_confirmed - globalCases[i].total_deaths - globalCases[i].total_recovered : globalCases[i].total_active_cases);
+                
+                if(globalCases[i].population_density >= 1000){
+                    globalCases[i].population_density = (globalCases[i].population_density /1000).toFixed(1)
+                }
+                    //let activeCases = (globalCases[i].total_active_cases == 0 ? globalCases[i].total_confirmed - globalCases[i].total_deaths - globalCases[i].total_recovered : globalCases[i].total_active_cases);
 
                 globalCases[i].total = {
                     confirmed : globalCases[i].total_confirmed,
-                    actives: activeCases,
+                        //actives: activeCases,
                     deaths: globalCases[i].total_deaths,
-                    recovered: globalCases[i].total_recovered
+                        //recovered: globalCases[i].total_recovered
                 }
                 globalCases[i].percentage ={
-                    actives : `${(activeCases / globalCases[i].total_confirmed * 100).toFixed(2)}%`,
+                        //actives : `${(activeCases / globalCases[i].total_confirmed * 100).toFixed(2)}%`,
                     deaths : `${(globalCases[i].total_deaths / globalCases[i].total_confirmed * 100).toFixed(2)}%`,
-                    recovered : `${(globalCases[i].total_recovered / globalCases[i].total_confirmed * 100).toFixed(2)}%`,
-                    mortality_rate: `${(globalCases[i].total_deaths / (globalCases[i].total_deaths + globalCases[i].total_recovered) * 100).toFixed(2)}%`,
+                        //recovered : `${(globalCases[i].total_recovered / globalCases[i].total_confirmed * 100).toFixed(2)}%`,
+                        //mortality_rate: `${(globalCases[i].total_deaths / (globalCases[i].total_deaths + globalCases[i].total_recovered) * 100).toFixed(2)}%`,
                 };
                 globalCases[i].global_percentage ={
                     confirmed : `${(globalCases[i].total_confirmed / globalConfirmedCases * 100).toFixed(2)}%`,
-                    actives : `${(activeCases / globalConfirmedCases * 100).toFixed(2)}%`,
+                        //actives : `${(activeCases / globalConfirmedCases * 100).toFixed(2)}%`,
                     deaths : `${(globalCases[i].total_deaths / globalConfirmedCases * 100).toFixed(2)}%`,
-                    recovered : `${(globalCases[i].total_recovered / globalConfirmedCases * 100).toFixed(2)}%`,
+                        //recovered : `${(globalCases[i].total_recovered / globalConfirmedCases * 100).toFixed(2)}%`,
                     
                 };
+
+                //TODO realize why I did this
                 delete globalCases[i].total_confirmed ;
                 delete globalCases[i].total_active_cases;
                 delete globalCases[i].total_deaths;
@@ -99,13 +112,13 @@ module.exports.getCasesPerDate = async (req, res, next) => {
                     longitude: globalCases[i].longitude
                 }
 
-                if(globalCases[i].country == "DIAMOND PRINCESS"){
+                if(globalCases[i].country_region == "DIAMOND PRINCESS"){
                     globalCases[i].coordinates = {
                         latitude: "35.41458",
                         longitude: "139.68203"
                     }
                 }
-                if(globalCases[i].country == "BELIZE"){
+                if(globalCases[i].country_region == "BELIZE"){
                     globalCases[i].coordinates = {
                         latitude: "17.1907805",
                         longitude: "-88.3596575"
